fdd0a51c0eaa1e8a472194ec1d4bafa8d6ce27b3
Ivan Moreno <ivan.moreno1@g.austincc.edu>
2018-04-05 10:33:03 -0500

    added material ui

--- food-fight/src/App.js
+++ food-fight/src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">FoodFight</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This App is to rank the undisputed champs in three different food categories:
+          Tacos, Burgers, and BBQ in Austin, TX.
         </p>
       </div>
     );
