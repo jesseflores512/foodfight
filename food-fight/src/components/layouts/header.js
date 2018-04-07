20e795918aa8206c1d4f65c2411fbacb52069a29
Ivan Moreno <ivan.moreno1@g.austincc.edu>
2018-04-07 14:00:51 -0500

    added layout folder w/ files

---
+++ food-fight/src/components/layouts/header.js
@@ -0,0 +1,10 @@
+import React from 'React'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+
+export default props =>
+  <AppBar position="static">
+    <Toolbar>
+      
+    </Toolbar>
+  </AppBar>
---
+++ food-fight/src/components/layouts/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom"/;
+
+const App = () => (
+  <div>
+    <h1>Tacos, Burges, and BBQ</h1>
+  </div>
+)
+
+render(<App />, document.getElementById("root"))
