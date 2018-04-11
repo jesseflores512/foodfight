3e3a4c2cc83e364ef9244e687219922862e938f0
jesseflores512 <jesseflores512@gmail.com>
2018-04-10 19:17:28 -0500

    edited paths on App.js

--- food-fight/src/App.js
+++ food-fight/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
-import Header from './Components/Layouts/Header';
-import Main from './Components/Layouts/Main';
-import Subcomponents from './Components/subcomponents'
+import Header from './components/layouts/header';
+import Main from './components/layouts/Main';
+import Subcomponents from './components/subcomponents'
 
 export default class extends Component {
   render() {
