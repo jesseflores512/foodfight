0a58632e2550a7b612234666ec93147159122322
jesseflores512 <jesseflores512@gmail.com>
2018-04-12 20:42:49 -0500

    div buttons

--- food-fight/src/Components/subcomponents/BBQ.js
+++ food-fight/src/Components/subcomponents/BBQ.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../../App.css'
 
+const test = ()=> {
+  console.log('it works dawg')
+}
+
 export default class Bbq extends React.Component {
   render(){
     return (
-        <div className='ButtonMain ButtonBbq'>
+        <div className='ButtonMain ButtonBbq' onClick = {test}>
           <h1 className='ButtonHead'>BBQ</h1>
         </div>
     );
