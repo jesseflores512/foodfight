ece24c4a5e821925d84cb2b19d7efdfa05e52c42
jesseflores512 <jesseflores512@gmail.com>
2018-04-12 19:39:54 -0500

    css button

--- food-fight/src/Components/subcomponents/BBQ.js
+++ food-fight/src/Components/subcomponents/BBQ.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../../App.css'
 
 export default class Bbq extends React.Component {
   render(){
     return (
-      <div>
-        <button>BBQ</button>
+      <div className='ButtonMain'>
+        <h1 className='ButtonHead'>BBQ</h1>
       </div>
     );
   }
