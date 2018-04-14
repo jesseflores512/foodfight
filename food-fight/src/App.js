c6a6292a9a389ce6a1b8aa60bda155868e097e03
jesseflores512 <jesseflores512@gmail.com>
2018-04-13 19:47:54 -0500

    added rankings component

--- food-fight/src/App.js
+++ food-fight/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/layouts/header'
 import Main from './components/layouts/Main'
+import Ranking from './components/layouts/rankings'
+
 export default class App extends React.Component {
   render() {
     return(
       <div>
         <Header />
-        <Main />
+        {/* <Main /> */}
+        <Ranking />
       </div>
     );
   }
---
+++ food-fight/src/components/layouts/rankings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Ranking extends React.Component {
+  render(){
+    return (
+      <div>
+        <h2>test</h2>
+
+      </div>
+    );
+  }
+}
