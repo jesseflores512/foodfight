6c213d6008ebf6032c6f7e2d589dfd71cfda792a
jesseflores512 <jesseflores512@gmail.com>
2018-04-15 16:41:33 -0500

    react router installed

--- food-fight/src/App.js
+++ food-fight/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from './components/layouts/header'
-import Main from './components/layouts/main'
+import Main from './components/layouts/Main'
 import Ranking from './components/layouts/rankings'
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 export default class App extends React.Component {
   render() {
     return(
-      <div>
-        <Header />
-        {/* <Main /> */}
-        <Ranking />
-      </div>
+      <Router>
+        <div>
+          <Header />
+          {/* <Main /> */}
+          <Ranking />
+        </div>
+      </Router>
     );
   }
 }
