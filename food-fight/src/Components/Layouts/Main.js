df337be56ebaa2c60a41f60e1fbe43ea0c3ee889
Ivan Moreno <ivan.moreno1@g.austincc.edu>
2018-04-15 18:42:17 -0500

    changed name

--- food-fight/src/Components/Layouts/Main.js
+++ food-fight/src/Components/Layouts/Main.js
@@ -21,19 +21,19 @@ class Ranking extends React.Component {
   render(){
     return (
       <div>
-        <h1>2018 RANKINGS</h1>
-        <h2>Tacos</h2>
+        <h1 className="rankHeading">2018 RANKINGS</h1>
+        <h2 className="rankHeading">Tacos</h2>
           <p>Taco Champ</p>
           <p>Torchy's Tacos</p>
         <br/>
           <div className="myButton">Nominate a challenger</div>
         <hr/>
-        <h2>Burgers</h2>
+        <h2 className="rankHeading">Burgers</h2>
           <p>Burger Champ</p>
           <p>Hopdoddy</p>
         <br/>
           <div className="myButton">Nominate a challenger</div>
-        <h2>BBQ</h2>
+        <h2 className="rankHeading">BBQ</h2>
           <p>BBQ Champ</p>
           <p>LA Barbeque</p>
         <br/>
