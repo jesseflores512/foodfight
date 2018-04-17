0252b362b06374a472786d35383dd0c11914f394
Ivan Moreno <ivan.moreno1@g.austincc.edu>
2018-04-17 10:14:56 -0500

    updated newchallenger page

--- food-fight/src/components/layouts/newchallenger.js
+++ food-fight/src/components/layouts/newchallenger.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './header'
+import { Form, Text } from "react-form";
 
 export default class NewChallenger extends React.Component {
   render() {
@@ -8,7 +9,49 @@ export default class NewChallenger extends React.Component {
     <Header/>
       <h3 className="NewChallenger">Nominate New Challenger</h3>
 
-        <div className="myButton">Nominate a challenger</div>
+       <Form
+         render={({ submitForm, values, addValue, removeValue }) => (
+           <form onSubmit={submitForm}>
+             <Text field="restaurantName" placeholder="Restaurant Name" />
+             <Text field="address" placeholder="Address" />
+             <Text field="city" placeholder="City" />
+             <Text field="state" placeholder="State" />
+             <Text field="zipCode" placeholder="Zip Code" />
+             <Text field="lastName" placeholder="Last Name" />
+             <div>
+               Information Card
+               {values.friends &&
+                 values.friends.map((friend, i) => (
+                   <div>
+                     <Text
+                       field={["friends", i, "restaurantName"]}
+                       placeholder="Restaurant Name"
+                     />
+                     <Text
+                       field={["friends", i, "address"]}
+                       placeholder="Address"
+                     />
+                     <Text
+                       field={["friends", i, "state"]}
+                       placeholder="State"
+                     />
+                     <Text
+                       field={["friends", i, "zipCode"]}
+                       placeholder="Zip Code"
+                     />
+                     <Text
+                       field={["friends", i, "lastName"]}
+                       placeholder="Last Name"
+                     />
+
+                   </div>
+                 ))}
+                 <div className="myButton">Nominate a challenger</div>
+             </div>
+           </form>
+            )}
+          />
+
     </div>
   )}
 }
