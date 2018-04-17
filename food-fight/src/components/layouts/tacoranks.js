a9fcfef77d68f6b8f20872a462a0454e5ceadb38
Ivan Moreno <ivan.moreno1@g.austincc.edu>
2018-04-16 20:23:22 -0500

    updated rankings pages

--- food-fight/src/components/layouts/tacoranks.js
+++ food-fight/src/components/layouts/tacoranks.js
@@ -6,21 +6,9 @@ export default class TacoRanks extends React.Component {
     return (
       <div>
       <Header />
-        <h1 className="rankHeading">2018 RANKINGS</h1>
         <h2 className="rankHeading">Tacos</h2>
           <p>Taco Champ</p>
           <p>Torchy's Tacos</p>
-        <br/>
-          <div className="myButton">Nominate a challenger</div>
-        <hr/>
-        <h2 className="rankHeading">Burgers</h2>
-          <p>Burger Champ</p>
-          <p>Hopdoddy</p>
-        <br/>
-          <div className="myButton">Nominate a challenger</div>
-        <h2 className="rankHeading">BBQ</h2>
-          <p>BBQ Champ</p>
-          <p>LA Barbeque</p>
         <br/>
           <div className="myButton">Nominate a challenger</div>
       </div>
