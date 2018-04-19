3465ba280211e1959f780e03def003eb3ad48616
jesseflores512 <jesseflores512@gmail.com>
2018-04-19 15:48:09 -0500

    got post to work on rankings page

--- food-fight/src/components/layouts/bbqranks.js
+++ food-fight/src/components/layouts/bbqranks.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import Header from './header'
+import axios from 'axios';
 
 export default class BbqRanks extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.tabRow = this.tabRow.bind(this);
+      this.state = {serverports: [],
+                    name: ''};
+    }
+    componentDidMount(){
+      axios.get('http://localhost:4200/serverport')
+      .then(response => {
+        this.setState({ serverports: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+    }
+    tabRow(){
+        return this.state.serverports.map(function(object, i){
+            return (<li>{object.name} location: {object.address} {object.city}, {object.state}</li>);
+        });
+    }
+
   render(){
     return (
       <div>
       <Header />
         <h2 className="rankHeading">BBQ</h2>
-          <p>BBQ Champ</p>
-          <p>LA Barbeque</p>
+          <ol>
+            {this.tabRow()}
+          </ol>
         <br/>
         <a href = '/challenger'>
           <div className="myButton">Nominate a challenger</div>
